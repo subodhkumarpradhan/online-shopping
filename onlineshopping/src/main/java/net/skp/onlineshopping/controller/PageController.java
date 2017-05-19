9ea97c0e20e4475dabbe6a01ec8a56c9d39d382c
Subodh Kumar <subodh.msu@gmail.com>
2017-05-19 17:38:17 +0530

    Successfully Loaded Static Resources

---
+++ onlineshopping/src/main/java/net/skp/onlineshopping/controller/PageController.java
@@ -0,0 +1,20 @@
+package net.skp.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	
+	@RequestMapping(value = {"/", "/home", "/index"})
+	public ModelAndView index(){
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Welcome to Spring Web MVC");
+		return mv;
+	}
+	
+}
