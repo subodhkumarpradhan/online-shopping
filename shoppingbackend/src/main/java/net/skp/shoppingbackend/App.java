cf499aa6d19cbecc9a056ba0754df0b61b7a4184
Subodh Kumar Pradhan <subodh.msu@gmail.com>
2023-08-06 11:12:09 +0530

    commit 7

--- shoppingbackend/src/main/java/net/skp/shoppingbackend/App.java
+++ shoppingbackend/src/main/java/net/skp/shoppingbackend/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!", "How are you all" );
+        System.out.println( "Hello World!", "Bye" );
     }
 }
